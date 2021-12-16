f8aa48b5b3a9ddf7dfcd8a30807f8f0daf5246ec
Osoze <101652548+Osoze@users.noreply.github.com>
2021-12-16 22:50:24 +0000

    add tooltip

--- build.js
+++ build.js
@@ -37,4 +37,4 @@ gh.get("orgs/github/members", { all: true }, function (err, data) {
         return function (next) {
             gh.get("users/" + c.login, function (err, data) {
                 if (err) {
-                    
+                    Logger.log("Failed to fetch the name for @
