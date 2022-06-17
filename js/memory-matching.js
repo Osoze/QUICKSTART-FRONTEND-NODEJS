8cb51bfc3907cbd1ec322eb761dbd843a9871e19
Osoze <101652548+Osoze@users.noreply.github.com>
2022-06-17 10:06:36 +0000

    Remove debugging info

--- js/memory-matching.js
+++ js/memory-matching.js
@@ -51,3 +51,44 @@ function shuffle(array) {
     while (0 !== currentIndex) {
 
         // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function selectCard() {
+	if ($(".card-flipped").size() > 1) {
+		return;
+	}
+	$(this).addClass("card-flipped");
+	if ($(".card-flipped").size() == 2) {
+		setTimeout(checkPattern,1400);
+	}
+}
+
+function checkPattern() {
+	if (isMatchPattern()) {
+		$(".card-flipped").removeClass("card-flipped").addClass("card-removed");
+		$(".card-removed").bind("webkitTransitionEnd",removeTookCards);
+	} else {
+		$(".card-flipped").removeClass("card-flipped");
+	}
+}
+
+function isMatchPattern() {
+	var cards = $(".card-flipped");
+	var pattern = $(cards[0]).data("pattern");
+	var anotherPattern = $(cards[1]).data("pattern");
+	return (pattern == anotherPattern);
+}
+
+function removeTookCards() {
+	$(".card-removed").remove();
+	if ($(".card").length == 0) {
